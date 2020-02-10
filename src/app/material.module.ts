0d6af49e2afb89f6231b0de286a18a08442e6c29
Baptiste <baptiste.mahe@alfresco.com>
2020-02-10 22:16:12 +0000

    Sections as Expansion Panels

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -4,13 +4,15 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatListModule } from '@angular/material/list';
 import { MatTabsModule } from '@angular/material/tabs';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 @NgModule({
   exports: [
     MatToolbarModule,
     MatDividerModule,
     MatListModule,
-    MatTabsModule
+    MatTabsModule,
+    MatExpansionModule
   ]
 })
 export class MaterialModule { }
