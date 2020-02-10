5870acf163d8e6b4676e1155b03a260b72fd677e
Baptiste <baptiste.mahe@alfresco.com>
2020-02-10 21:58:33 +0000

    Sections as mat-tab components

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatDividerModule } from '@angular/material/divider';
-import { MatListModule } from '@angular/material/list'
+import { MatListModule } from '@angular/material/list';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @NgModule({
   exports: [
     MatToolbarModule,
     MatDividerModule,
-    MatListModule
+    MatListModule,
+    MatTabsModule
   ]
 })
 export class MaterialModule { }
--- src/app/section-container/section-container.component.ts
+++ src/app/section-container/section-container.component.ts
@@ -7,12 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SectionContainerComponent implements OnInit {
 
+  // tslint:disable-next-line: max-line-length
+  lorem = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ullamcorper ut sapien ac porttitor. Nullam scelerisque arcu vitae dignissim viverra. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Ut tellus elit, rhoncus id odio at, elementum tincidunt turpis. Sed in lacus pretium, ultrices nunc sagittis, convallis ante. Morbi gravida, metus sit amet mattis posuere, turpis urna porta sapien, non cursus mauris elit ac ipsum. Suspendisse tempus, lectus quis auctor dictum, turpis nisl mollis ipsum, posuere tincidunt est sem blandit lacus. Nullam maximus eget nibh tincidunt facilisis. Donec mattis dui sed dapibus porta.';
+
   sectionContentArray = [{
-    imageUrl: "../../assets/img-exemple.png",
-    paragraph: "This is the content of the paragraph"
+    label: 'Introduction to the site',
+    imageUrl: '../../assets/img-example.png',
+    paragraph: this.lorem
   }, {
-    imageUrl: "../../assets/img-exemple.png",
-    paragraph: "This is the content of the paragraph"
+    label: 'Projects',
+    imageUrl: '../../assets/img-example.png',
+    paragraph: this.lorem
+  }, {
+    label: 'Professional experiences',
+    imageUrl: '../../assets/img-example.png',
+    paragraph: this.lorem
+  }, {
+    label: 'Education',
+    imageUrl: '../../assets/img-example.png',
+    paragraph: this.lorem
+  }, {
+    label: 'Personal',
+    imageUrl: '../../assets/img-example.png',
+    paragraph: this.lorem
   }];
 
   constructor() { }
