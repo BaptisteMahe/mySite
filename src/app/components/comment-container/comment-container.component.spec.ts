14206673838736eb63b2671f5e88407205383988
Baptiste <baptiste.mahe@alfresco.com>
2020-02-12 00:57:13 +0000

    First attemps fot comment section

---
+++ src/app/components/comment-container/comment-container.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CommentContainerComponent } from './comment-container.component';
+
+describe('CommentContainerComponent', () => {
+  let component: CommentContainerComponent;
+  let fixture: ComponentFixture<CommentContainerComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ CommentContainerComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CommentContainerComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
