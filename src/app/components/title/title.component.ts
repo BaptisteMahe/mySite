1e2408a22a2a2bec07dcef860f1da06c69fd01ac
Baptiste <baptiste.mahe@alfresco.com>
2020-02-14 09:16:29 +0000

    Added title animations

--- src/app/components/title/title.component.ts
+++ src/app/components/title/title.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { fromEvent, Observable} from 'rxjs';
+import { throttleTime, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-title',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TitleComponent implements OnInit {
 
+  scrollObs: Observable<any>;
+  initialHeight = 100;
+  verticalPos: number;
+
+
   constructor() { }
 
   ngOnInit() {
+    // Ask about Observables and pipe of those.
+    this.scrollObs = fromEvent(window, 'scroll').pipe(
+      throttleTime(10),
+      map(() => window.pageYOffset)
+    );
+
+    this.scrollObs.subscribe(verticalPos => {
+      this.verticalPos = this.initialHeight / (verticalPos / 2 + 1);
+    });
   }
 
 }
